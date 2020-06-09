fada4d86e8d38a4a2e5dd3964ac9b180ee50c3b4
claire gautier <claire.gautier@edhec.com>
2020-06-09 09:34:49 +0200

    created and tested restaurant odel

--- db/migrate/20200609072528_add_address_to_restaurants.rb
+++ db/migrate/20200609072528_add_address_to_restaurants.rb
@@ -1,6 +1,5 @@
 class AddAddressToRestaurants < ActiveRecord::Migration[6.0]
   def change
-    #add_column :table, :column, :type
     add_column :restaurants, :address, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_071516) do
+ActiveRecord::Schema.define(version: 2020_06_09_072528) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
